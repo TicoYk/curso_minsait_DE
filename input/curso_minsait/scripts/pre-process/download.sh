8ef6672cb0dba508e4821499d23c91970f331976
Dídimo Yokoyama de Sousa <didimoykym@gmail.com>
2023-05-27 11:22:43 +0000

    adição de lóigica para baixar arquivos csv

--- input/curso_minsait/scripts/pre-process/download.sh
+++ input/curso_minsait/scripts/pre-process/download.sh
@@ -1,5 +1,15 @@
 
 cd ../../raw
-mkdir categoria
-cd categoria
-curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+
+mkdir temp
+curl -s https://github.com/caiuafranca/dados_curso > ./temp/arquivos.html
+linkRaw="https://raw.githubusercontent.com/caiuafranca/dados_curso/main/"
+
+grep -o -E 'title="([^"]+).csv"' ./temp/arquivos.html | grep -o -E '".+"' | grep -o -E '[a-zA-Z/./_]{0,}' > ./temp/arquivos.txt
+
+mkdir arquivos
+for file in $(cat "./temp/arquivos.txt")
+do
+  echo "$linkRaw$file"
+  curl -o "arquivos/$file" "$linkRaw$file"
+done
